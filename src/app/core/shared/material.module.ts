3179162cfeb374e5855b558f7faba5d12a9a33fa
Antal Visser <antalvisser@gmail.com>
2021-06-19 09:26:30 +0200

    Add material datepicker

--- src/app/core/shared/material.module.ts
+++ src/app/core/shared/material.module.ts
@@ -1,26 +1,53 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatCardModule } from '@angular/material/card';
-import { MatSortModule } from '@angular/material/sort';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, MatIconModule,
-    MatButtonModule, MatTableModule, MatPaginatorModule, MatProgressSpinnerModule,
-    MatCardModule, MatSortModule, MatGridListModule, DragDropModule, MatSlideToggleModule],
-  exports: [MatFormFieldModule, MatSelectModule, MatInputModule, MatIconModule,
-    MatButtonModule, MatTableModule, MatPaginatorModule, MatProgressSpinnerModule,
-    MatCardModule, MatSortModule, MatGridListModule, DragDropModule, MatSlideToggleModule]
+  imports: [
+    DragDropModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatSortModule,
+    MatTableModule
+  ],
+  exports: [
+    DragDropModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatSortModule,
+    MatTableModule
+  ]
 })
 export class MaterialModule {
   static forRoot(): ModuleWithProviders<MaterialModule> {
